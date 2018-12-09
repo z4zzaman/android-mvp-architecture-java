cccc5742f9125372ed5c88fa2bd4cba81b409be7
z4zzaman <zzaman08@gmail.com>
2018-12-09 19:49:19 +0600

    first commit

---
+++ app/src/main/java/com/monir/framework/mvp/BaseApp.java
@@ -0,0 +1,17 @@
+package com.monir.framework.mvp;
+
+/*
+ *  ****************************************************************************
+ *  * Created by : Md. Moniruzzaman Monir on 12/8/2018 at 2:32 PM.
+ *  * Email : zzaman08@gmail.com
+ *  *
+ *  * Purpose:
+ *  *
+ *  * Last edited by : Md. Moniruzzaman Monir on 12/8/2018.
+ *  *
+ *  * Last Reviewed by : <Reviewer Name> on <mm/dd/yy>
+ *  ****************************************************************************
+ */
+
+public class BaseApp {
+}
---
+++ app/src/main/java/com/monir/framework/mvp/ui/base/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.monir.framework.mvp.ui.base;
+
+/*
+ *  ****************************************************************************
+ *  * Created by : Md. Moniruzzaman Monir on 12/8/2018 at 2:30 PM.
+ *  * Email : zzaman08@gmail.com
+ *  *
+ *  * Purpose:
+ *  *
+ *  * Last edited by : Md. Moniruzzaman Monir on 12/8/2018.
+ *  *
+ *  * Last Reviewed by : <Reviewer Name> on <mm/dd/yy>
+ *  ****************************************************************************
+ */
+
+public class BaseActivity {
+}
